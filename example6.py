aa94c7dc49279210be6f565b204d0ffcc0f6b98d
Stephan Skanta <78694630+techcree@users.noreply.github.com>
2022-12-05 16:17:04 +0100

    Add files via upload

---
+++ example6.py
@@ -0,0 +1,11 @@
+#by ssk TechCree
+#lern micropython Example 5
+import os
+
+def show_details(name=None, age=None):
+    print(name, age)
+    
+name = "Stephan"
+age = 41
+    
+show_details(name, age)
---
+++ example7.py
@@ -0,0 +1,21 @@
+#by ssk TechCree
+#lern micropython Example 5
+import os
+import utime
+
+while True:
+    print("Hello Stephan")
+    utime.sleep(1)
+    #break
+    #continue
+    pass
+
+#or try
+
+#count = 0
+
+#while count < 4:
+#    print("test", count)
+#    utime.sleep(1)
+#    count = count +1
+    
